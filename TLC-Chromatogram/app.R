b99e486dd31d8a170e72ba8061dfe4e6e11781f0
Anson C <orangejuicethegreat@gmail.com>
2024-04-24 02:04:40 +0100

    Working clunky reactive chromatogram removed tbd features, add later

--- TLC-Chromatogram/app.R
+++ TLC-Chromatogram/app.R
@@ -1,10 +1,12 @@
 library(shiny)
 library(bslib)
+library(rhandsontable)
 library(DT)
 library(tidyverse)
 library(ggplot2)
 
-##FUNCTIONS##
+
+##FUNCTIONS-------------
 #Easy: when deltaRf >= 0.2; Hard: when deltaRf < 0.2
 #Efficiency, N
 N_easy <- function(mass_loading){
@@ -39,15 +41,11 @@ elution_curve <- function(x, Vr, Xa, bandsize) {
   Xa * exp(-(x - Vr)^2/(bandsize/2.8)^2)
 }
 
-# Define UI
+#-----------
+#UI---------
 ui <- fluidPage(
-  # Page title
   title = "Chromatogram Simulator",
-  
-  # Navbar
   page_fluid(
-    title = "Chromatogram Simulator",
-    
     # Content
     layout_columns(
       # Chromatogram
@@ -75,48 +73,19 @@ ui <- fluidPage(
       navset_card_underline(
         title = "Data entry",
         
-        # If the user wants to plot experimental Rfs ----
+        # Experimental Isocratic Mode ----
         nav_panel(
           "Empirical Rf",
           id = "empirical_isocratic",
           numericInput("crude_mass", "Crude Mass (g)", 0.5, min = 0),
-          DTOutput("rf_table"),
-          layout_columns(
-            actionButton("add_row_rf_table", "Add Row"),
-            actionButton("delete_row_rf_table", "Delete Row")
-          ),
-          verbatimTextOutput("results")
+          rHandsontableOutput("rftable"),
+          verbatimTextOutput("debugger")
         ),
         
-        # If the user wants to use predicted Rfs ----
+        # Prediction Isocratic Mode ----
         nav_panel(
           "Predicted Rf",
-          id = "predicted",
-          sliderInput(
-            "strong_solvent_percent",
-            "Eluent, Strong solvent%",
-            min = 0,
-            max = 100,
-            value = 1,
-            ticks = FALSE,
-            animate = TRUE
-          ),
-          p(
-            "Step 1: Provide the necessary Rf data for each spot. Each Rf column represents a different strong solvent%. Add at least three data points for each spot for accurate prediction!"
-          ),
-          DTOutput("prediction_table"),
-          layout_columns(
-            actionButton("add_row_pred_table", "Add Row"),
-            actionButton("delete_row_pred_table", "Delete Row")
-          ),
-          p("Step 2: Provide corresponding strong solvent% values for the Rfs."),
-          layout_columns(
-            numericInput("rf_1", "Rf_1", value = 0),
-            numericInput("rf_2", "Rf_2", value = 0),
-            numericInput("rf_3", "Rf_3", value = 0),
-            numericInput("rf_4", "Rf_4", value = 0),
-            numericInput("rf_5", "Rf_5", value = 0)
-          )
+          id = "predicted"
         )
       ),
       
@@ -131,130 +100,81 @@ ui <- fluidPage(
       ),
       col_widths = c(8, 4, 8, 4)
     )
-  ))
+  )
+)
 
-# Define server logic
+
+#-------------
+#SERVER-------
 server <- function(input, output, session) {
   
-  # ##USER DATA FOR RF PREDICION
-  # user_prediction_data <- reactiveVal(
-  #   data.frame(
-  #     "Spot_Number" = c(1, 2, 3),
-  #     "Rf_1" = c(0.1, 0.1, 0.2),
-  #     "Rf_2" = rep(NA, 3),
-  #     "Rf_3" = rep(NA, 3),
-  #     "Rf_4" = rep(NA, 3),
-  #     "Rf_5" = rep(NA, 3),
-  #     "Xa" = c(0.2, 0.2, 0.4)
-  #   )
-  # )
-  
-  # output$prediction_table <- renderDT({
-  #   # Display the reactive dataframe as an editable table
-  #   datatable(user_prediction_data(), editable = TRUE, rownames = FALSE, selection = 'single', options = list(dom = 't', ordering = F))
-  # })
-  
   ##USER TLC DATA INPUT
-  # Create a reactive dataframe to store the table data
-  user_tlc_data <- reactiveVal(data.frame(
-    "Rf" = c(0.7, 0.5, 0.25),
-    "Xa" = c(0.2, 0.2, 0.6)
+  # Default TLC data for editable table
+  default_tlc <- reactive(data.frame(
+    "Rf" = c(0.71, 0.4, 0.2),
+    "Xa" = c(0.2, 0.5, 0.3)
   ))
-  
   # Render the editable Rf table
-  output$rf_table <- renderDT({
-    # Display the reactive dataframe as an editable table
-    datatable(
-      user_tlc_data(),
-      editable = TRUE,
-      rownames = FALSE,
-      selection = 'single',
-      options = list(dom = 't', ordering = F)
-    )
+  output$rftable <- renderRHandsontable({
+    rhandsontable(default_tlc(), selectCallback=TRUE, readOnly = FALSE)
   })
   
-  # Update the dataframe when the user edits the table
-  observeEvent(input$rf_table_cell_edit, {
-    info <- input$rf_table_cell_edit
+  #--------------------
+  #CALCULATIONS -------
+  
+  
+  rv <- reactiveValues(tlc_data = NULL,
+                       is_hard = NULL,
+                       efficiency = NULL,
+                       silica_mass = NULL,
+                       Vm = NULL,
+                       Vr = NULL,
+                       bandsize = NULL,
+                       plotdata = data.frame("solvent" = rep(0, length = 500))
+                       )
+  
+  observe({ rv$tlc_data <- hot_to_r(input$rftable)
+    rv$is_hard <- any(diff(sort(rv$tlc_data$Rf)) < 0.2)
+    #Calculate efficiency of each spot (N) and column silica mass
+    if (rv$is_hard == TRUE) {
+      rv$efficiency <- N_hard(rv$tlc_data$Rf)
+      rv$silica_mass <- siog_hard(input$crude_mass)
+    } else {
+      rv$efficiency <- N_easy(rv$tlc_data$Rf)
+      rv$silica_mass <- siog_easy(input$crude_mass)
+    }
+    #Calculate void volume (Vm), retention volume (Vr), bandsize
+    rv$Vm <- calc_Vm(rv$silica_mass)
+    rv$Vr <- calc_Vr(rv$Vm, rv$tlc_data$Rf)
+    rv$bandsize <- calc_bandsize(rv$Vr, rv$efficiency)
     
-    # Update the reactive dataframe based on user edits
-    newData <- user_tlc_data()
-    newData[info$row, info$col] <- info$value
-    user_tlc_data(newData)
-  })
-  
-  #CALCULATIONS AND PLOTTING---
-  
-  plotdf <- reactive({
-    isolate({
-      #make a copy of the user-inputted data to perform manipulations on
-      new_data <- isolate(user_tlc_data())
-      
-      # Calculate if the separation is hard
-      is_hard <- reactiveVal(any(diff(sort(user_tlc_data()$Rf)) < 0.2))
-
-      # Calculate efficiency of each spot (N) and column silica mass
-      if (is_hard() == TRUE) {
-        new_data$efficiency <- N_hard(new_data$Rf)
-        silica_mass <- siog_hard(input$crude_mass)
-      } else {
-        new_data$efficiency <- N_easy(new_data$Rf)
-        silica_mass <- siog_easy(input$crude_mass)
-      }
-
-      # Calculate void volume
-      Vm <- calc_Vm(silica_mass)
-
-      # Calculate retention volume
-      new_data$Vr <- calc_Vr(Vm, new_data$Rf)
-
-      # Calculate band size
-      new_data$bandsize <- calc_bandsize(new_data$Vr, new_data$efficiency)
-
-      # Create X variables
-      plotdf <- data.frame("solvent" = seq(0, Vm * 5, length = 500))
-
-      # Calculate Spot columns
-      for (i in 1:nrow(user_tlc_data())) {
+    rv$plotdata <- data.frame("solvent" = seq(0, rv$Vm * 5, length = 500))
+    if (!is.null(nrow(rv$tlc_data))){
+      for (i in 1:nrow(rv$tlc_data)) { 
         # Create column names
         spot_col <- paste("Spot", i, sep = "_")
-
-        # Calculate values using the formula for current row
-        values <- elution_curve(plotdf$solvent, new_data$Vr[i], new_data$Xa[i], new_data$bandsize[i])
-
+        
         # Assign values to new column
-        plotdf[[spot_col]] <- values
+        rv$plotdata[spot_col] <- elution_curve(rv$plotdata$solvent, rv$Vr[i], rv$tlc_data$Xa[i], rv$bandsize[i])
       }
-
-      # Convert to long format so that it's ggplot compatible
-      #Should turn it from
-      # solvent spot 1 spot 2 spot 3
-      #   0       0     0.1     0
-      #   1       0     0.2     0.1 
-      # To
-      # solvent name  value
-      #   0     spot1   0
-      #   0     spot2   0.1
-      #   0     spot3   0   
-      #   1     spot1   0 
-      # etc etc
-      plotdf <- pivot_longer(plotdf, cols = starts_with("Spot"))
-      
-      return(plotdf)
-    })
-  })
+    #Convert to long format for ggplot2
+    rv$plotdata <- pivot_longer(rv$plotdata, cols = starts_with("Spot"))
+    }
+  }) %>%
+  bindEvent({ #make it update only when the table or crude_mass is updated
+    input$rftable
+    input$crude_mass
+    }, label = "calculations")
   
-  #Output to see what the datatable looks like. For some reason, renderDT doesn't work on plotdf(), not sure what's happening
-  output$results <- renderText({paste(head(plotdf()))})
-  
-  # View the resulting dataframe
+  # output$debugger <- renderText(
+  #   paste(str(rv$plotdata))
+  # )
+  #--------------
+  #PLOTTING------
   output$chromatogram <- renderPlot({
-    #Calculations
-    
-    ggplot(data=plotdf(),
-           aes(x=solvent, y=value, colour=name)) +
-      geom_line() +
-      labs( x = "Eluent (mL)", y = "Relative peak intensity")
+    ggplot(data=rv$plotdata, aes(x = solvent, y = value, colour = name)) +
+        geom_line() +
+        labs( x = "Eluent (mL)", y = "Relative peak intensity")
   })
   
 }
