4ac77949c3948334b1da4449f9dacbadbf4b0828
Anson C <orangejuicethegreat@gmail.com>
2024-04-29 16:56:15 +0100

    Added fraction size sppt and faster chromatogram calculations

--- TLC-Chromatogram/app.R
+++ TLC-Chromatogram/app.R
@@ -24,8 +24,8 @@ siog_hard <- function(crude_mass){
 }
 
 # Column void volume (Vm)
-calc_Vm <- function(siog){
-  return(1.8*siog+0.3)
+calc_Vm <- function(SiOg){
+  return(1.8*SiOg+0.3)
 }
 # Retention volume (Vr)
 calc_Vr <- function(Vm, rf) {
@@ -39,6 +39,10 @@ calc_bandsize <- function(Vm, efficiency){
 #Elution curve calculation (y)
 elution_curve <- function(x, Vr, Xa, bandsize) {
   Xa * exp( -(x - Vr)^2 / (bandsize/2.8)^2 )
+}
+#gen layers for plot
+line_layer_add <- function(plot, funct, label){
+  plot + geom_function(fun = funct, aes(colour = label), n = 500)
 }
 
 #-----------
@@ -57,7 +61,7 @@ ui <- fluidPage(
         nav_panel( "TLC", plotOutput(outputId = "tlc") ),
         nav_panel( #HTML( paste0("R", tags$sub("s") ) )
                   "Peak Info", 
-                  rHandsontableOutput("resolution") 
+                  rHandsontableOutput("resolution")
                   )
       ),
       
@@ -70,9 +74,7 @@ ui <- fluidPage(
           "Empirical Rf",
           id = "empirical_isocratic",
           numericInput("crude_mass", "Crude Mass (g)", 0.5, min = 0),
-          rHandsontableOutput("rftable"),
-          paste("debugger table"),
-          rHandsontableOutput("debugger")
+          rHandsontableOutput("rftable")
         ),
         
         # Prediction Isocratic Mode ----
@@ -90,7 +92,7 @@ ui <- fluidPage(
         ),
         numericInput("user_silica", "Silica (g):", value = 0),
         verbatimTextOutput("silica"),
-        numericInput("user_fraction_size", "Fraction size (mL):", value = 0),
+        numericInput("user_fraction_size", "Fraction size (mL):", value = 0, min = 0),
         verbatimTextOutput("fraction_size")
       ),
       col_widths = c(8, 4, 8, 4)
@@ -120,23 +122,22 @@ server <- function(input, output, session) {
   
   rv <- reactiveValues(tlc_data = NULL,
                        is_hard = NULL,
-                       efficiency = NULL,
                        silica_mass = NULL,
                        Vm = NULL,
-                       Vr = NULL,
-                       bandsize = NULL,
                        plotdata = data.frame("solvent" = rep(0, length = 500)),
-                       sep = NULL
+                       sep = NULL,
+                       comb_plot = NULL,
+                       fraction_size = NULL
                        )
   
   observe({ rv$tlc_data <- hot_to_r(input$rftable)
     rv$is_hard <- any(diff(sort(rv$tlc_data$Rf)) < 0.2)
     #Calculate efficiency of each spot (N) and column silica mass
     if (rv$is_hard == TRUE) {
-      rv$efficiency <- N_hard(rv$tlc_data$Xa)
+      rv$tlc_data$efficiency <- N_hard(rv$tlc_data$Xa)
       rv$silica_mass <- siog_hard(input$crude_mass)
     } else {
-      rv$efficiency <- N_easy(rv$tlc_data$Xa)
+      rv$tlc_data$efficiency <- N_easy(rv$tlc_data$Xa)
       rv$silica_mass <- siog_easy(input$crude_mass)
     }
     #if user supplies silica amt
@@ -145,42 +146,41 @@ server <- function(input, output, session) {
     }
     #Calculate void volume (Vm), retention volume (Vr), bandsize
     rv$Vm <- calc_Vm(rv$silica_mass)
-    rv$Vr <- calc_Vr(rv$Vm, rv$tlc_data$Rf)
-    rv$bandsize <- calc_bandsize(rv$Vr, rv$efficiency)
+    rv$tlc_data$Vr <- calc_Vr(rv$Vm, rv$tlc_data$Rf)
+    rv$tlc_data$bandsize <- calc_bandsize(rv$tlc_data$Vr, rv$tlc_data$efficiency)
     
-    rv$plotdata <- data.frame("solvent" = seq(0, rv$Vm * 10, length = 500))
+    #make sure the rows are calculated before this is done
     if (!is.null(nrow(rv$tlc_data))){
-      for (i in 1:nrow(rv$tlc_data)) { 
-        # Create column names
-        spot_col <- paste("Spot", i, sep = "_")
-        
-        # Assign values to new column
-        rv$plotdata[spot_col] <- elution_curve(rv$plotdata$solvent, rv$Vr[i], rv$tlc_data$Xa[i], rv$bandsize[i])
-      }
-    #Convert to long format for ggplot2
-    rv$plotdata <- pivot_longer(rv$plotdata, cols = starts_with("Spot"))
-    
-    #Calculate other data (Rs, fraction size calculation)
-    rv$sep <- data.frame("Rs" = 2*( rv$Vr - lag(rv$Vr) )/( rv$bandsize + lag(rv$bandsize) )) %>%
-      mutate( Separation =  case_when(
-        is.na(Rs) == TRUE ~ NA, 
-        Rs > 1.5 ~ "Good", 
-        Rs > 0.8 ~ "Moderate", 
-        .default = "Bad")
-        )
-    rv$sep <- data.frame(
-      "Peak_Start_mL" = rv$Vr - rv$bandsize/2,
-      "Peak_Middle_mL" = rv$Vr,
-      "Peak_End_mL" = rv$Vr + rv$bandsize/2
-      ) %>%
-      bind_cols(rv$sep, .)
-    #to-do: start middle end peak
+      rv$sep <- data.frame("Rs" = 2*( rv$tlc_data$Vr - lag(rv$tlc_data$Vr) )/( rv$tlc_data$bandsize + lag(rv$tlc_data$bandsize) )) %>%
+        mutate( Separation =  case_when(
+          is.na(Rs) == TRUE ~ NA, 
+          Rs > 1.5 ~ "Good", 
+          Rs > 0.8 ~ "Moderate", 
+          .default = "Bad")
+          ) 
+      rv$sep <- data.frame(
+        "Peak_Start_mL" = rv$tlc_data$Vr - rv$tlc_data$bandsize/2,
+        "Peak_Middle_mL" = rv$tlc_data$Vr,
+        "Peak_End_mL" = rv$tlc_data$Vr + rv$tlc_data$bandsize/2
+        ) %>%
+        bind_cols(rv$sep, .)
+      #to-do: start middle end peak
+      if (!is.na(input$user_fraction_size)) {
+        rv$fraction_size <- case_when(input$user_fraction_size != 0 ~ input$user_fraction_size, .default = rv$Vm/3)
+      } else { rv$fraction_size <- rv$Vm/3 }
+      
+      rv$sep <- mutate(rv$sep, Fraction_Start = Peak_Start_mL/rv$fraction_size, 
+                       Fraction_Mid = Peak_Middle_mL/rv$fraction_size, 
+                       Fraction_End = Peak_End_mL/rv$fraction_size,
+                       Fraction_sep = Fraction_Start - lag(Fraction_End))
     }
+    
   }) %>%
   bindEvent({ #make it update only when the table/crude_mass/user_silica is updated
     input$rftable
     input$crude_mass
     input$user_silica
+    input$user_fraction_size
     }, label = "calculations")
 
   output$resolution <- renderRHandsontable(
@@ -188,15 +188,8 @@ server <- function(input, output, session) {
     #output start middle end fractions
     rhandsontable(rv$sep, readOnly = TRUE)
   )
-  
-  #render text outputs for silica and fraction size
-
-  output$debugger <- renderRHandsontable({
-    debugtable <- cbind(rv$efficiency, rv$silica_mass, rv$Vm, rv$Vr, rv$bandsize)
-    colnames(debugtable) <- c("Efficiency", "Silica_Mass", "Vm", "Vr", "Bandsize")
-    return(rhandsontable(debugtable, readOnly = TRUE))
-    }
-   )
+  output$silica <- renderText(rv$silica_mass)
+  output$fraction_size <- renderText(rv$fraction_size)
   #--------------
   #PLOTTING------
   output$tlc <- renderPlot({
@@ -211,10 +204,27 @@ server <- function(input, output, session) {
   })
   
   output$chromatogram <- renderPlot({
-    plot <- ggplot(data=rv$plotdata, aes(x = solvent, y = value, colour = name)) +
-        geom_line() +
+    if (!is.null(rv$tlc_data$bandsize)){
+      # Generate list of functions to plot with all but x already applied
+      partials_list <- mapply(FUN=partial,
+                              Vr=rv$tlc_data$Vr,
+                              Xa=rv$tlc_data$Xa,
+                              bandsize=rv$tlc_data$bandsize,
+                              MoreArgs=list(`.f`=elution_curve))
+      
+      # Data to plot. Defining the domain for the plotted functions.
+      plot_data <- data.frame(x = seq(0, rv$Vm * 10, length.out = 10))
+      
+      # Initial plot object
+      init_plot <- ggplot(plot_data, aes(x=x))
+      
+      # Generate layers and accumulate to plot object with label
+      comb_plot <- reduce(.x = seq_along(partials_list),
+                          .f = function(p, i) line_layer_add(p, partials_list[[i]], paste0("Spot ", i)),
+                          .init = init_plot)
+      comb_plot +
         labs( x = "Eluent (mL)", y = "Relative peak intensity")
-    plot + scale_colour_brewer(palette = "Set1")
+    }
   })
   
 }
