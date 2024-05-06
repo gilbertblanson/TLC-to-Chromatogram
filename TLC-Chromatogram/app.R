421b663061c97704ccb25756ae8c81130d7ab977
Anson C <orangejuicethegreat@gmail.com>
2024-05-07 00:54:04 +0100

    UI overhaul, prediction backend (not functional yet)

--- TLC-Chromatogram/app.R
+++ TLC-Chromatogram/app.R
@@ -10,8 +10,8 @@ library(plotly)
 cbbPalette <- c( '#EE7733', '#0077BB', '#33BBEE', '#EE3377', '#CC3311', '#009988', '#BBBBBB')
 theme_set(theme_bw())
 
-
-##FUNCTIONS-------------
+#--------------------------------------------------------------------------------------------------------------------
+##FUNCTIONS----------------------------------------------------------------------------------------------------------
 #Easy: when deltaRf >= 0.2; Hard: when deltaRf < 0.2
 #Efficiency, N
 N_easy <- function(mass_loading){
@@ -51,14 +51,50 @@ line_layer_add <- function(plot, funct, label){
   plot + geom_function(fun = funct, aes(colour = label), n = 500)
 }
 
-#-----------
-#UI---------
+#--------------------------------------------------------------------------------------------------------
+#UI------------------------------------------------------------------------------------------------------
+accordion_filters <- accordion(
+  accordion_panel(
+    "Data Entry", icon = bsicons::bs_icon("menu-app"),
+    selectInput("data_entry", "Rf values:", c("Empirical", "Predicted"), selected = "Empirical" ),
+    conditionalPanel(
+      condition = "input.data_entry == 'Empirical'",
+      numericInput("crude_mass", "Crude Mass (g)", 0.5, min = 0),
+      tooltip(p("Compound data", bs_icon("info-circle") ),
+              p("Input your Rf and mass balance (Xa) data. You can add/remove spots by right clicking and navigating the pop-up menu.")
+      ),
+      rHandsontableOutput("rftable")
+    ),
+    conditionalPanel(
+      condition = "input.data_entry == 'Predicted'",
+      sliderInput("chosen_solvent_percent", "Strong solvent %", 1, 100, 30),
+      numericInput("no_spots", "Number of Spots", value = 2, min = 1, step = 1),
+      rHandsontableOutput("Xa_pred"),
+      rHandsontableOutput("data"),
+      textOutput("pred")
+    )
+  ),
+  accordion_panel(
+    "Optional Settings", tooltip(bs_icon("info-circle"), p("If you wish to set your own column size or fraction size, you can do so here. Otherwise, set to 0.")), 
+    icon = bsicons::bs_icon("sliders"),
+    
+    numericInput("user_silica", "Silica (g):", value = 0),
+    verbatimTextOutput("silica"),
+    numericInput("user_fraction_size", "Fraction size (mL):", value = 0, min = 0),
+    verbatimTextOutput("fraction_size")
+  )
+)
+
 ui <- fluidPage(
   title = "Chromatogram Simulator",
-  page_fluid(
-    # Content
+  page_sidebar(
+    sidebar = sidebar(accordion_filters, width = 300),
+    
+    #Main
     layout_columns(
-      # Chromatogram
+      col_widths = c(9, 3, 12),
+      row_heights = c(2, 1),
+      
       card(card_header("Chromatogram",
                        popover(
                          #Plot settings go here!
@@ -72,65 +108,30 @@ ui <- fluidPage(
                          radioButtons("frac_lines", "Show fraction lines:", 
                                       c("Yes" = "y", "No" = "n"), 
                                       selected = "n")
-                         ),
+                       ),
                        class = "d-flex justify-content-between"),
-           plotlyOutput(outputId = "chromatogram")
-           ),
-      
-      # Other information card
-      navset_card_underline(
-        nav_panel( "TLC", plotOutput(outputId = "tlc") ),
-        nav_panel( #HTML( paste0("R", tags$sub("s") ) )
-                  "Peak Info", 
-                  rHandsontableOutput("resolution")
-                  )
+           plotlyOutput(outputId = "chromatogram"),
+           max_height = 500
       ),
       
-      # Data Input
-      navset_card_underline(
-        title = "Data entry",
-        
-        # Experimental Isocratic Mode ----
-        nav_panel(
-          "Empirical Rf",
-          id = "empirical_isocratic",
-          numericInput("crude_mass", "Crude Mass (g)", 0.5, min = 0),
-          rHandsontableOutput("rftable")
-        ),
-        
-        # Prediction Isocratic Mode ----
-        nav_panel(
-          "Predicted Rf",
-          id = "predicted"
-        )
-      ),
-      
-      # Optional Settings
-      card(
-        card_header("Optional Settings", 
-                    tooltip(bs_icon("info-circle"), p("CIf you wish to set your own column size or fraction size, you can do so here. Otherwise, set to 0.")
-                            )
-                    ),
-        numericInput("user_silica", "Silica (g):", value = 0),
-        verbatimTextOutput("silica"),
-        numericInput("user_fraction_size", "Fraction size (mL):", value = 0, min = 0),
-        verbatimTextOutput("fraction_size")
-      ),
-      col_widths = c(8, 4, 8, 4)
+      card( card_header("TLC"), plotOutput(outputId = "tlc"), max_height = 500 ), 
+     
+      card( card_header("Separation details"), tableOutput("resolution") )
     ),
+    
+    #Gutter---------------------------------------------------------------------------------------------
     tags$div("This Shiny app is based off the work of J. D. Fair and C. M. Kormos, Journal of Chromatography A, 2008, 1211, 49–54.",tags$br(),
              "Rf prediction algorithm is based off of P. Kręcisz, K. Czarnecka and P. Szymański, Journal of Chromatographic Science, 2022, 60, 472–477.",
-               style = "font-size:10px;")
-    
+             style = "font-size:10px;")
   )
 )
 
 
-#-------------
-#SERVER-------
+#----------------------------------------------------------------------------------------------------------
+#SERVER----------------------------------------------------------------------------------------------------
 server <- function(input, output, session) {
   
-  ##USER TLC DATA INPUT
+  ##USER TLC DATA INPUT -- ISOCRATIC ---------------------------------------------------------------------------------------------
   # Default TLC data for editable table
   default_tlc <- reactive(data.frame(
     "Rf" = c(0.71, 0.4, 0.2),
@@ -141,8 +142,87 @@ server <- function(input, output, session) {
     rhandsontable(default_tlc(), selectCallback=TRUE, readOnly = FALSE)
   })
   
-  #--------------------
-  #CALCULATIONS -------
+  ##USER TLC DATA INPUT -- RF PREDICTION ---------------------------------------------------------------------------------------- 
+  
+  no_spots <- reactive(input$no_spots)
+  
+  pred_values <- reactiveValues(data = tibble("spot" = factor(c(1,1,1,2,2,2), levels = 1:2),
+                                              "solvent_percent" = c(30, 40, 50, 30, 40, 50),
+                                              "Rf" = c(0.16, 0.26, 0.41, 0.13, 0.21, 0.35) ),
+                                Xa = c(0.8, 0.2) )
+  
+  #update values
+  observe({
+    req(input$data)
+    pred_values$data = hot_to_r(input$data)
+  })
+  observe({
+    req(input$Xa_pred)
+    pred_values$Xa = dplyr::pull(hot_to_r(input$Xa_pred))
+  })
+  
+  observe({
+    req(input$no_spots)
+    
+    l_max <- max(as.numeric(levels(pred_values$data$spot)))
+    spot_diff <- input$no_spots - l_max
+    
+    if (input$no_spots > l_max ) {
+      
+      #increase levels of spot rows
+      levels(pred_values$data$spot) <-  1:input$no_spots
+      #increase mass balance spots
+      pred_values$Xa <-  append(pred_values$Xa, rep(0.5, spot_diff))
+      
+    } else if (input$no_spots < l_max ) {
+      #delete rows of larger spot no's & change levels of spot rows
+      pred_values$data <- filter(pred_values$data, as.numeric(spot) <= input$no_spots) %>%
+        droplevels()
+      levels(pred_values$data$spot) <-  1:input$no_spots
+      
+      #delete mass balance spots
+      pred_values$Xa <-  pred_values$Xa[1:input$no_spots]
+    }
+  })
+  
+  #regression function
+  perform_regression <- function(data) {
+    gsl_nls(Rf ~ (solvent_percent/phi_nought)^(-1/k), 
+            data, 
+            start = list(phi_nought = 8, k = -0.5),
+            algorithm = "lm",
+            control = list(scale = "levenberg"),
+            upper = list(k = 0),
+            lower = list(phi_nought = 0)) %>%
+      coef()
+  }
+  
+  # Group by spot and perform regression for each group
+  
+  predict_data_coef <- reactive( pred_values$data %>% group_split(spot) %>% map(perform_regression) )
+  
+  # Apply the function to each element of predict_data_coef
+  predicted_rf_values <- reactive({
+    predicted_rf_values <- sapply(predict_data_coef(), function(x) (input$chosen_solvent_percent / x[[1]])^(-1/x[[2]])) 
+    predicted_rf_values[predicted_rf_values > 1] <- 1
+    predicted_rf_values[predicted_rf_values < 0] <- 0
+    return(predicted_rf_values)
+  })
+  
+  #outputs
+  output$pred <- renderText(predicted_rf_values())
+  
+  output$Xa_pred <-  renderRHandsontable({
+    rhandsontable(tibble("Xa_pred" = pred_values$Xa)) 
+  })
+  
+  output$data <-  renderRHandsontable({
+    rhandsontable(pred_values$data) 
+  })
+  
+  
+  #-----------------------------------------------------------------------------------------------------------------
+  #CALCULATIONS ----------------------------------------------------------------------------------------------------
   
   
   rv <- reactiveValues(tlc_data = NULL,
@@ -208,17 +288,13 @@ server <- function(input, output, session) {
     input$user_fraction_size
     }, label = "calculations")
 
-  output$resolution <- renderRHandsontable(
-    #calculate fractions input$user_fraction_size
-    #output start middle end fractions
-    rhandsontable(rv$sep, readOnly = TRUE)
-  )
+  output$resolution <- renderTable(rv$sep)
   output$silica <- renderText(rv$silica_mass)
   output$fraction_size <- renderText(rv$fraction_size)
   
   
-  #--------------
-  #PLOTTING------
+  #-----------------------------------------------------------------------------------------------------------
+  #PLOTTING---------------------------------------------------------------------------------------------------
   output$tlc <- renderPlot({
     no_spots <- nrow(rv$tlc_data)
     tlc <- ggplot(data=rv$tlc_data, aes(x = rep(0, no_spots), y = Rf, colour = as.factor(1:no_spots) )) +
